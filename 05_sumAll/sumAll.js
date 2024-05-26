3938f19fbfe25fa41e09e38c074e7ab8ac4ff6f3
Bradley Eversley <16165703+BradleyEversley@users.noreply.github.com>
2024-05-26 06:33:52 -1000

    added codebase for sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
 
-};
+
+const sumAll = function(x, y) {
+
+let sum = 0;
+
+    //swaps if y is smaller than x
+    if (y < x) {
+        const temp = x;
+        x = y;
+        y = temp;
+    } 
+    if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR"; // valid numbers only
+    }
+   
+    for (let i = x; i <= y; i++) {
+        sum += i;
+    }
+        return sum;
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
