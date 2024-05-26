7a66b37ad6f8244e4b55a83f80ad4f41baa4d83a
Bradley Eversley <16165703+BradleyEversley@users.noreply.github.com>
2024-05-26 01:58:38 -1000

    PAssed all 7 tests for string repeater

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let result = '';
+    let count = 0;
+
+    if (num >=0) {
+        while (count < num) {
+            result += string;
+            count ++;
+        }
+    } else {
+        result = 'ERROR';
+    }
+
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
