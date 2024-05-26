7241420b3d87bdc402421078ed164ddbad9a6507
Bradley Eversley <16165703+BradleyEversley@users.noreply.github.com>
2024-05-26 04:51:13 -1000

    added codebase for array item removal

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+
+// rest parameter, takes in an array and a variable number of arguments (...args)
+const removeFromArray = function(array,...exclude) {
+
+    const newArray = []; //empty box
+
+    //"converting a for loop to forEach" - mdn webdocs
+    //"array.prototype.push()" - mdn webdocs
+    array.forEach(element => {
+        
+        if (!exclude.includes(element)) { //if 'element' is not included in "exclude"
+            newArray.push(element); //then put it into our new box called newArray
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
