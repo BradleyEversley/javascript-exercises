ddc90067dce1b6c793f97104d811ba6cfa3c4f8d
Bradley Eversley <16165703+BradleyEversley@users.noreply.github.com>
2024-05-27 09:44:35 -1000

    added codebase for leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+ return year % 4 == 0 && (year % 100 !== 0 || year % 400 == 0)
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// true if it is the case that year is div by 4 AND is
+// true if the year is either div by 400 or not div by 100
