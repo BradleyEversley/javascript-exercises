5ed605234086556869c05e495ba8ca45d7fdf8d7
Bradley Eversley <16165703+BradleyEversley@users.noreply.github.com>
2024-06-04 21:12:42 -1000

    added codebase to pass palindrome npm tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const curatedString = string.toLowerCase().split('').filter((character) => alphanumeric.includes(character)).join('');
+
+    const reversedString = curatedString.split('').reverse().join('');
+
+    return curatedString === reversedString;
+  
 };
 
 // Do not edit below this line
